05b0cabe3dfb1e64c44845f4ce59d7259246ef83
ferzxk <mmiernik47@gmail.com>
2024-12-02 23:00:37 +0100

    update Login.js handleSubmit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,54 @@
 import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(`Logging with: ${username} and pw: ${password}`);   // PRINT
+
+        try {
+            const response = await fetch('http://localhost:5000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json' 
+                },
+                body: JSON.stringify({username, password})
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Logged in:', data);
+            } else {
+                console.error('Error', data.message);
+            }
+        } catch (err){
+            console.error('Error', err);
+        }
+    }
+
     return (
         <div className='flex justify-center items-center h-screen bg-gray-100'>
-            <form className='bg-white p-6 rounded-lg shadow-md w-96'>
+            <form onChange={handleSubmit} className='bg-white p-6 rounded-lg shadow-md w-96'>
                 <h2 className='text-2xl font-bold mb-4'>Login</h2>
                 <div className="mb-4">
                     <label className='block mb-2 text-gray-700'>Username</label>
-                    <input type='text' className="w-full p-2 border rounded-md" required/>
+                    <input type='text' value={username} onChange={handleUsernameChange} className='w-full p-2 border rounded-md' placeholder='Enter username' required/>
                 </div>
                 <div className='mb-4'>
                     <label className='block mb-2 text-gray-700'>Password</label>
-                    <input type='password' className='w-full p-2 border rounded-md' required/>
+                    <input type='password' value={password} onChange={handlePasswordChange} className='w-full p-2 border rounded-md' placeholder='Enter password' required/>
                 </div>
                 <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600'>Log in</button>
             </form>
