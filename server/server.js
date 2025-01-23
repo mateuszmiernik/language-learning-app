8634e331b0f99a633e6b9dee079e092d86050a01
ferzxk <mmiernik47@gmail.com>
2025-01-23 10:52:44 +0100

    update server.js by removing useNewUrlParser

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const flashcardRoutes = require('./routes/flashcardRoutes'); // Import tras fisz
 const app = express();
 
 // Połącz się z MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Failed to connect to MongoDB:', err));
 
